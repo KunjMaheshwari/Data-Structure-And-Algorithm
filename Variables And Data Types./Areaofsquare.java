603888f90451eb24dbc157929eacbc2410e7920c
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-01-09 20:25:23 +0530

    Add files via upload

---
+++ Variables And Data Types./Areaofsquare.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Areaofsquare {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int area = input * input;
+        System.out.println(area);
+    }
+}
---
+++ Variables And Data Types./Average.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Average {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int average = (A + B + C) / 3;
+        System.out.println(average);
+    }
+}
