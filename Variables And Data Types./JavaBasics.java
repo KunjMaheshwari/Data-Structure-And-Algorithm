ce0eb153ce7df0511dad54f1f4a9805b1dd91ed1
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-01-09 20:08:38 +0530

    Variables And Data Types in Java

---
+++ Variables And Data Types./JavaBasics.java
@@ -0,0 +1,6 @@
+public class JavaBasics { // class name must be equal to the file name
+    public static void main(String args[]) {
+        System.out.println("Java Programming");
+        System.out.println("Hello World"); // ; is called statement terminator
+    }
+}
