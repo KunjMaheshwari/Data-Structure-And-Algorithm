a32275adcc6b0d413f88ceb2db38077cd39a56ac
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-01-15 12:45:30 +0530

    Add files via upload

---
+++ Operators/Logical.java
@@ -0,0 +1,7 @@
+public class Logical {
+    public static void main(String args[]) {
+        System.out.println((3 > 2) && (5 > 0)); // in && if both the statements are true then the output will be true.
+        System.out.println((3 < 2) || (5 > 0)); // in || if any one statement is true then the output wil be true.
+        System.out.println(!(3 < 2)); // ! changes true into false and vice versa.
+    }
+}
---
+++ Operators/Realtional.java
@@ -0,0 +1,12 @@
+public class Realtional {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
+        System.out.println(a >= b);
+    }
+}
