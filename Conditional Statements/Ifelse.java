4db1d3bd91b9a504625fb58bf94e5d665cf0509e
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-01-15 12:57:09 +0530

    Create Ifelse.java

---
+++ Conditional Statements/Ifelse.java
@@ -0,0 +1,10 @@
+public class Ifelse{
+  public static void main(String args[]){
+    int age = 22;
+    if (age>=18){
+      System.out.println("Adult: Drive: Vote");
+    }else{
+      System.out.println("Not an Adult");
+    }
+  }
+}
