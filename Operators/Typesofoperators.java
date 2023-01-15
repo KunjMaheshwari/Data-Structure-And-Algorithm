2b31237db5a32f7ed5472873569e0280c52f5dbe
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-01-15 12:45:10 +0530

    Create Typesofoperators.java

---
+++ Operators/Typesofoperators.java
@@ -0,0 +1,14 @@
+public class Typesofoperators {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b; // a and b are operands and + is the operator
+        int multiply = a * b;
+        int divide = a / b;
+        int remainder = a % b;
+        System.out.println(sum);
+        System.out.println(multiply);
+        System.out.println(divide);
+        System.out.println(remainder);
+    }
+}
