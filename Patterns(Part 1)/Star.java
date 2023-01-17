0fd5cf9d019180e8d169e2148f1d14fe7293e6ee
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-01-18 02:09:29 +0530

    Create Star.java

---
+++ Patterns(Part 1)/Star.java
@@ -0,0 +1,10 @@
+public class Star{
+  public static void main(String args[]){
+    for(int j = 1;j<=4;j++){
+      for(int star =1; star<=j; star++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
