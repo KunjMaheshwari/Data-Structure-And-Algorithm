782b2fa11b49e1a691b83b6982e7f75932328639
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-01-26 14:09:56 +0530

    Add files via upload

--- Functions/Factorial.java
+++ Functions/Factorial.java
@@ -1,5 +1,19 @@
-public class Factorial {
-    public static void main(String args[]) {
+import java.util.*;
 
+public class Factorial {
+    public static int facto(int a) {
+        int f = 1;
+        for (int i = a; i >= 1; i--) {
+            f = f * i;
+        }
+        return f;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        int a = sc.nextInt();
+        int fact = facto(a);
+        System.out.println("Factorial of the number is: " + fact);
     }
 }
