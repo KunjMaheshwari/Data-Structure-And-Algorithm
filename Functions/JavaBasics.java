534c827374a9cb6cd45489f33f403bc72560386b
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-01-26 12:23:13 +0530

    Create JavaBasics.java

---
+++ Functions/JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics {
+  public static void printHelloWorld(){
+    System.out.println("Hello World");
+  }
+  public static void main(String args[]){
+    printHelloWorld();
+  }
+}
