5765f3528f3eacc52d633956330cbd0e17035b1d
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-02-06 12:05:59 +0530

    Create Strings.java

---
+++ Strings/Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String args[]) {
+        // These are the ways to declare the strings in an array
+        char arr[] = { 'a', 'b', 'c', 'd' };
+        String str = "abcd";
+        String str2 = new String("xyz");
+    }
+}
+// IMPORTANT - In java, Strings are Immutable.
