b11a056f30d7ac8dd5ca7095ea71f4881fd32a95
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-02-06 12:13:48 +0530

    Input and Output in Strrings

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -1,9 +1,17 @@
+import java.util.*;
+
 public class Strings {
     public static void main(String args[]) {
         // These are the ways to declare the strings in an array
         char arr[] = { 'a', 'b', 'c', 'd' };
         String str = "abcd";
         String str2 = new String("xyz");
+        
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.next();    // For strings we use sc.next()
+        // here sc.next() will print only one word whereas sc.nextLine() will print the complete sentence. 
+        System.out.println(name);
     }
 }
 // IMPORTANT - In java, Strings are Immutable.
