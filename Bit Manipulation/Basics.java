1e732b2441086c61a98675ac47a050af9595b586
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-02-10 13:14:34 +0530

    Added Binary AND,OR,XOR Operator

---
+++ Bit Manipulation/Basics.java
@@ -0,0 +1,10 @@
+public class Basics{
+  public static void main(String args[]){
+    System.out.println((5 & 6));  // & is Binary AND Operator
+    // Output of the above code will be 4.
+    System.out.println((5|6)); // | is Binary OR Operator
+    // Output of the above code will be 7.
+    System.out.println((5^6)); // ^ is the Binary XOR Operator
+    // Output of the above code will be 3. 
+  }
+}
