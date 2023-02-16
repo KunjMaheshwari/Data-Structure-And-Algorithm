d5dd372fa79481f373a728a604ef5cb53b449547
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-02-16 15:25:39 +0530

    Add files via upload

---
+++ Object Oriented Programming (OOPS)/Encapsulation.java
@@ -0,0 +1,10 @@
+/*
+ * Four pillars of Object oriented Programming are :
+ * 1. Encapsulation
+ * 2. Inheritance
+ * 3. Abstraction
+ * 4. Polymorphism
+ * 
+ * Encapsulation is defined as the wrapping up of data and methods under a
+ * single unit. It also implements data hiding.
+ */
