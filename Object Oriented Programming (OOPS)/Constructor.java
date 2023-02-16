04300161db778979c404197e3bd61a7f85f88b78
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-02-16 15:47:09 +0530

    Add files via upload

---
+++ Object Oriented Programming (OOPS)/Constructor.java
@@ -0,0 +1,40 @@
+/*
+ * Constructor is a special method which is invoked automatically at the time of
+ * object creation.
+ * Constructor have a same name as class
+ * Constructor don't have a return type.
+ * Constructor are only called once, at object creation.
+ * Memory allocation happens when constructor is called.
+ */
+
+public class Constructor {
+    public static void main(String args[]) {
+        Student s1 = new Student(21);
+        System.out.println(s1.rollnumber);
+        Student s2 = new Student("Kunj Maheshwari");
+        System.out.println(s2.name);
+        Student s3 = new Student();
+        System.out.println();
+        // Student s4 = new Student("Prakhar Sharma", 48);
+        // System.out.println(s4.name, s4.rollnumber);
+        // The above code will give an error coz there is no constructor with the
+        // parameters name and rollnumber.
+    }
+}
+
+class Student {
+    String name;
+    int rollnumber;
+
+    Student() {
+        // This is the non parameterized constructor.
+    }
+
+    Student(String name) { // This is a parameterized constructor.
+        this.name = name;
+    }
+
+    Student(int rollnumber) {
+        this.rollnumber = rollnumber;
+    }
+}
