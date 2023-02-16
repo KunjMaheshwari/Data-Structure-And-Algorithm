29f1592752862d7f175750d833bd4b43dce59c8f
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-02-16 15:14:54 +0530

    Added Getters and Setters

--- Object Oriented Programming (OOPS)/Oops.java
+++ Object Oriented Programming (OOPS)/Oops.java
@@ -1,35 +1,62 @@
-public class Oops{
-  public static void main(String args[]){   // By convention main should be public and static 
-    Pen p1 = new Pen();   // We have created a pen object called p1 and this is called Constructor
-    p1.setColor("Blue");
-    System.out.println(p1.color);
-    p1.setTip(5);
-    System.out.println(p1.tip);
-  }
+public class Oops {
+    public static void main(String args[]) { // By convention main should be public and static
+        Pen p1 = new Pen(); // We have created a pen object called p1 and this is called Constructor
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
+
+        // BankAccount myaccount = new Backaccount();
+        // myaccount.username = "Kunj";
+        // myaccount.setPassword("abcd");
+    }
 }
 
-class Pen{
-  // Attributes + functions
-  String color;
-  int tip;
-  
-  void setColor(String newColor){
-    color = newColor;
-  }
-  void setTip(int newTip){
-    tip = newTip;
-  }
+// class BankAccount {
+// public String username;
+// private String password;
+
+// public void setPassword(String pwd) {
+// password = pwd;
+// }
+// }
+
+class Pen {
+    // Attributes + functions
+    private String color;
+    private int tip;
+
+    // GETTER
+    String getColor() {
+        return this.color; // this keyword is used to refer to the current object.
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    // SETTER
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int newTip) {
+        this.tip = newTip;
+    }
 }
 
-class Student{
-  String name;
-  int age;
-  float percentage;
-  
-  void setname(String newName){
-    name = newName;
-  }
-  void calpercentage(int phy, int chem, int maths){
-    percentage = (phy+chem+maths)/3;
-  }
-}
+// class Student {
+// String name;
+// int age;
+// float percentage;
+
+// void setname(String newName) {
+// name = newName;
+// }
+
+// void calpercentage(int phy, int chem, int maths) {
+// percentage = (phy + chem + maths) / 3;
+// }
+// }
