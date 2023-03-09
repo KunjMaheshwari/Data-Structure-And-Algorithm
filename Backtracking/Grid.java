7006dcd44f8ee1ec3632845a6f7d6f3f5e289e79
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-03-10 00:24:54 +0530

    Add files via upload

---
+++ Backtracking/Grid.java
@@ -0,0 +1,22 @@
+public class Grid {
+    // here i and j are the positions where we are standing in the grid
+    public static int gridways(int i, int j, int n, int m) {
+        // base case
+        if (i == n - 1 && j == m - 1) { // condition for the last cell
+            return 1;
+        } else if (i == n || j == m) {
+            return 0;
+        }
+
+        int w1 = gridways(i + 1, j, n, m);
+        int w2 = gridways(i, j + 1, n, m);
+        return w1 + w2;
+    }
+
+    public static void main(String args[]) {
+        int n = 3, m = 3;
+        System.out.println(gridways(0, 0, n, m));
+    }
+}
+
+// Time complexity of the code will be O(2^(n+m))
