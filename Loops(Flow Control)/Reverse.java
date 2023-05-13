52326de0bb941df997a373262d64a31a43bb00e9
Kunj Maheshwari <93459418+KunjMaheshwari@users.noreply.github.com>
2023-05-13 22:09:10 +0530

    Update Reverse.java

--- Loops(Flow Control)/Reverse.java
+++ Loops(Flow Control)/Reverse.java
@@ -1,10 +1,12 @@
 public class Reverse {
     public static void main(String args[]) {
         int n = 20122002;
+        int rev = 0;
         while (n > 0) {
             int lastDigit = n % 10;
-            System.out.print(lastDigit + " ");
-            n = n / 10;
+            rev = (rev*10) + lastDigit;
+            n = n/10;
         }
+        System.out.println(rev);
     }
 }
